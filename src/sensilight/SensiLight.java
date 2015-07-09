c5cc0ca9e63a14deb911ee2655f0239cce04349b
akrv <aswinkarthik.rv@gmail.com>
2015-07-09 20:11:12 +0200

    cleaned the code, dist build for windows 8.1
    working with change colors

--- src/sensilight/SensiLight.java
+++ src/sensilight/SensiLight.java
@@ -5,16 +5,15 @@
  */
 package sensilight;
 
-import java.awt.AWTException;
-import java.awt.Color;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.color.ColorSpace;
-import java.awt.image.BufferedImage;
+
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import de.androidpit.colorthief.ColorThief;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 
 /**
@@ -31,54 +30,15 @@ public class SensiLight {
     static SerialPort serialPort;
     public static void main(String[] args) throws SerialPortException, AWTException{
     Runtime.getRuntime().addShutdownHook(new Message());
-    int[] dataBuffInt;
-    int counter = 0;
-//    int sumR = 0;
-//    int sumG = 0;
-//    int sumB = 0;
-//    int avgR,avgG,avgB;
     byte[] buffer;
-//    int h,w,imageLen;
-    //int[] redz,greenz,bluez;
-    buffer = new byte[11];
     SerialInit("COM6");
-    
     while (true){
        
     //start of while loop
-    //long startTime = System.nanoTime();
     BufferedImage capture = getImage();
-//    h = capture.getHeight();
-//    w = capture.getWidth();
-//    imageLen = w*h;
-//    dataBuffInt = capture.getRGB(0, 0, w, h, null, 0, w);
     int[] color = ColorThief.getColor(capture,5,true);
-//    int[] redz = new int[imageLen];
-//    int[] greenz = new int[imageLen];
-//    int[] bluez = new int[imageLen];
-//    
-//    for (int i=0; i<dataBuffInt.length;i++){
-//        Color c1 = new Color(dataBuffInt[i]);
-//        Color c = c1.brighter();
-//        redz[i] = c.getRed();
-//        greenz[i] = c.getGreen();
-//        bluez[i] = c.getBlue();
-//        }
-//
-//    for (int i = 0;i<imageLen; i++){
-//        sumR += redz[i];
-//        sumG += greenz[i];
-//        sumB += bluez[i];
-//    }
-//    avgR = sumR/imageLen;
-//    avgG = sumG/imageLen;
-//    avgB = sumB/imageLen;
     String values = color[0]+","+color[1]+","+color[2]+".";
-//    buffer[0]= (byte) avgR;
-//    buffer[1]= (byte) avgG;
-//    buffer[2]= (byte) avgB;
     buffer = values.getBytes();
-    //System.out.println(values);
     boolean writeBytes = serialPort.writeBytes(buffer);
     }   
 }
