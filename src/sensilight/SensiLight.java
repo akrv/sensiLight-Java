f08c14f8fcc7e47f0ba33c5383c55a77c00fbd9c
akrv <aswin@southpaw.in>
2015-07-09 14:57:44 +0200

    added color thief androidpit.de
    creating dominant colour with quality 5 and ignoring white gives a
    performance of 54 milliseconds

--- src/sensilight/SensiLight.java
+++ src/sensilight/SensiLight.java
@@ -10,9 +10,11 @@ import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import jssc.SerialPort;
 import jssc.SerialPortException;
+import de.androidpit.colorthief.ColorThief;
 
 
 /**
@@ -28,54 +30,53 @@ public class SensiLight {
     static SerialPort serialPort;
     public static void main(String[] args) throws AWTException, SerialPortException{
     int[] dataBuffInt;
-    int sumR = 0;
-    int sumG = 0;
-    int sumB = 0;
-    int avgR,avgG,avgB;
+//    int sumR = 0;
+//    int sumG = 0;
+//    int sumB = 0;
+//    int avgR,avgG,avgB;
     byte[] buffer;
-    int h,w,imageLen;
+//    int h,w,imageLen;
     //int[] redz,greenz,bluez;
-    buffer = new byte[3];
-    SerialInit("COM6");
+    buffer = new byte[11];
+    //SerialInit("COM6");
     while (true){
     //start of while loop
-    long startTime = System.currentTimeMillis();
+    long startTime = System.nanoTime();
     Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
     BufferedImage capture = new Robot().createScreenCapture(screenRect);
-    h = capture.getHeight();
-    w = capture.getWidth();
-    imageLen = w*h;
-    dataBuffInt = capture.getRGB(0, 0, w, h, null, 0, w);
-    int[] redz = new int[imageLen];
-    int[] greenz = new int[imageLen];
-    int[] bluez = new int[imageLen];
-    
-    for (int i=0; i<dataBuffInt.length;i++){
-        Color c1 = new Color(dataBuffInt[i]);
-        Color c = c1.brighter();
-        redz[i] = c.getRed();
-        greenz[i] = c.getGreen();
-        bluez[i] = c.getBlue();
-        }
-
-    for (int i = 0;i<imageLen; i++){
-        sumR += redz[i];
-        sumG += greenz[i];
-        sumB += bluez[i];
-    }
-    
-    avgR = sumR/imageLen;
-    avgG = sumG/imageLen;
-    avgB = sumB/imageLen;
-    String values = avgR+","+avgG+","+avgB+".";
-    
-    
-
-    buffer[0]= (byte) avgR;
-    buffer[1]= (byte) avgG;
-    buffer[2]= (byte) avgB;
+//    h = capture.getHeight();
+//    w = capture.getWidth();
+//    imageLen = w*h;
+//    dataBuffInt = capture.getRGB(0, 0, w, h, null, 0, w);
+    int[] color = ColorThief.getColor(capture,5,true);
+//    int[] redz = new int[imageLen];
+//    int[] greenz = new int[imageLen];
+//    int[] bluez = new int[imageLen];
+//    
+//    for (int i=0; i<dataBuffInt.length;i++){
+//        Color c1 = new Color(dataBuffInt[i]);
+//        Color c = c1.brighter();
+//        redz[i] = c.getRed();
+//        greenz[i] = c.getGreen();
+//        bluez[i] = c.getBlue();
+//        }
+//
+//    for (int i = 0;i<imageLen; i++){
+//        sumR += redz[i];
+//        sumG += greenz[i];
+//        sumB += bluez[i];
+//    }
+//    avgR = sumR/imageLen;
+//    avgG = sumG/imageLen;
+//    avgB = sumB/imageLen;
+    String values = color[0]+","+color[1]+","+color[2]+".";
+//    buffer[0]= (byte) avgR;
+//    buffer[1]= (byte) avgG;
+//    buffer[2]= (byte) avgB;
     buffer = values.getBytes();
+    
     boolean writeBytes = serialPort.writeBytes(buffer);
+    System.out.println(System.nanoTime()-startTime);
     }
     
    }
